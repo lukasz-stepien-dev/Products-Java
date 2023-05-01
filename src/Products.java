f2044b856e52affb681372c0259e1183d1309fd9
Łukasz Stępień <lukaszatepien@gmail.com>
2023-05-01 21:37:45 +0200

    Make static list of products in Products.java

--- src/Products.java
+++ src/Products.java
@@ -3,19 +3,17 @@ import java.util.Objects;
 
 public class Products
 {
+    private static Products list[];
     private static int id = 0;
     private String name;
     private long amount;
     private LocalDate deliveryDay;
     private LocalDate expirationDate;
 
-    private int checkYear(int y) {
-        if (y == 0) {
-            return LocalDate.now().getYear();
-        }
-        else {
-            return y;
-        }
+    private int checkYear(int y)
+    {
+        if (y == 0) { return LocalDate.now().getYear(); }
+        else { return y; }
     }
 
     public Products(String n, long a, int dDelivery, int mDelivery, int yDelivery, int dExp, int mExp, int yExp)
@@ -43,26 +41,29 @@ public class Products
     public LocalDate getDeliveryDay() {
         return deliveryDay;
     }
-
     public LocalDate getExpirationDate() {
         return expirationDate;
     }
-
+    public static Products[] getList() {
+        return list;
+    }
     public void setName(String name) {
         this.name = name;
     }
-
     public void setAmount(long amount) {
         this.amount = amount;
     }
-
     public void setDeliveryDay(final int d, final int m, int y) {
         y = checkYear(y);
         this.deliveryDay = LocalDate.of(y, m, d);
     }
-
     public void setExpirationDate(final int d, final int m, int y) {
         y = checkYear(y);
         this.expirationDate = LocalDate.of(y, m, d);
+    }
+
+    public static void addToList(Products newProduct) {
+        int length = Products.list.length;
+        Products.list[length] = newProduct;
     }
 }
