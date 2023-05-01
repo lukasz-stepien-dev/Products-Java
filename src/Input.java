8f540e5fd725c242fb00ab26a8e35a42a60bae57
Łukasz Stępień <lukaszatepien@gmail.com>
2023-05-01 21:03:03 +0200

    Make Input.java that can get String from user

---
+++ src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input
+{
+    static Scanner scanner = new Scanner(System.in);
+    public static String in()
+    {
+        return scanner.next();
+    }
+}
